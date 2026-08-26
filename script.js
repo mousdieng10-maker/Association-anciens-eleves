228868ce82f6dd58d81ca736946f46ccf806becc
Moustapha Dieng <mousdieng10@gmail.com>
2026-08-26 08:25:37 -0400

    updates to darkmode

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const body = document.getElementById("body");
 const navDroite = document.querySelector("#navDroite");
 const greytxt = document.querySelector("#greytxt");
 const contacts = document.querySelectorAll(".contact");
+const imgs = document.querySelectorAll("img");
+const nav = document.querySelector("nav");
+const even = document.querySelector("#Evenements");
+const firstContacts = document.querySelectorAll(".firstContact")
+const sectionTitres = document.querySelectorAll(".sectionTitre")
 function hide(element){
     element.style.display = "none";
 }
@@ -31,9 +36,21 @@ lightToggle.onclick = function(){
     body.style.background = "#303030";
     body.style.color = "white";
     greytxt.style.color = "#bdbdbd";
+    for(const image of imgs){
+        image.style.boxShadow = `1px 1px 10px rgba(0, 0, 0, 0.6)`
+    }
+    for(const sectionTitre of sectionTitres){
+        sectionTitre.style.color = "gold";
+    }
+    nav.style.boxShadow = `1px 1px 10px rgba(0, 0, 0, 0.6)`;
     for(const contact of contacts){
         contact.style.color = "white"
     }
+    for(const first of firstContacts){
+        first.style.color = "#6e88fa"
+    }
+    even.style.background = `linear-gradient(70deg, rgba(0,0,0,.5),rgba(0,0,0,.5)),url("assets/silhouette.jpg")`
+    even.style.backgroundSize  = "cover";
     hide(lightToggle)
     const darkToggle = document.createElement("i")
     darkToggle.classList.add("fa-solid");
@@ -47,6 +64,14 @@ lightToggle.onclick = function(){
         for(const contact of contacts){
             contact.style.color = "black"
         }
+        for(const image of imgs){
+            image.style.boxShadow = `1px 1px 10px rgba(0, 0, 0, 0.6)`
+        }
+        for(const first of firstContacts){
+            first.style.color = "blue"
+        }
+        nav.style.boxShadow = `1px 1px 10px rgba(0, 0, 0, 0.4)`;
+        
         greytxt.style.color = "#383838";
         hide(darkToggle);
         visible(lightToggle);
